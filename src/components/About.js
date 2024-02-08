1771968098edbe3346c2027c3da96382df9f8fd5
Malik Najeeb Ul Habib <maliknajeeb113@gmail.com>
2024-02-08 15:12:47 +0530

    Revert "image fix"
    This reverts commit 04432d057eee26d222f7bc15cc8bb96314ba4f51.

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,17 @@
-import najeeb from "../assets/images/najeeb-2.png";
+import najeeb from "../assets/images/najeeb.jpg";
 
 const About = () => {
   return (
     <div className="bg-[rgb(3,15,10)]" id="about">
       <div className="container px-8 lg:mx-auto  flex flex-col h-screen items-center pt-14 text-left">
-        <h1 className="text-6xl text-white font-Magilio ">Yes, This is Me</h1>
-        <div className="flex pt-10 flex-col lg:flex-row w-full">
+        <h1 className="text-6xl text-white font-Magilio ">Experience</h1>
+        <div className="flex pt-10 flex-row w-full">
           {/* Work Experience */}
-          
+          <div className="flex lg:w-2/5 items-center justify-center">
+            <img src={najeeb} className="h-96" alt="me" />
+          </div>
           <div className="flex flex-col gap-7">
+            ÏÏ
             <div className="font-Satoshi text-4xl text-yellow-500 ">
               Rubrik Inc.
               <div className="text-white text-2xl">
